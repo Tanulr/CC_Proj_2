578b022db1b00a5fb40279a1d271d2f03670c7af
thejaswiamarendra <thejaswia03@gmail.com>
2023-04-09 12:42:01 +0530

    buttons added

--- rabbitmq/producer/producer.py
+++ rabbitmq/producer/producer.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import pika, os, logging, time
 logging.basicConfig()
 
@@ -28,35 +28,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello, this is our first flask website"
+    return render_template('index.html')
 
-@app.route('/one')
+@app.route('/one', methods=['GET'])
 def healthcheck():
     bodys = "Consumer 1"
     channel.basic_publish(exchange='', routing_key='one', body = bodys)
     print("[x] message sent to consumer = "+bodys)
-    return True
+    return "healthcheck"
 
 @app.route('/two')
 def insert():
     bodys = "Consumer 2"
     channel.basic_publish(exchange='', routing_key='two', body = bodys)
-
-    return True
+    return "insert"
+    
 
 @app.route('/three')
 def delete():
     bodys = "Consumer 3"
     channel.basic_publish(exchange='', routing_key='three', body = bodys)
 
-    return True
+    return "delete"
 
 @app.route('/four')
 def read():
     bodys = "Consumer 4"
     channel.basic_publish(exchange='', routing_key='four', body = bodys)
 
-    return True
+    return "read"
 
 if __name__ =='__main__':
     app.run(debug = True)
